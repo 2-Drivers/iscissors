daad2f921a410fd9b27a640d0f6cab236e8c6b54
JdotX <jxiaab@ust.hk>
2016-02-18 20:22:42 +0800

    2016-02-18 20:22

--- src/Algorithm/ImageNodeStructure.java
+++ src/Algorithm/ImageNodeStructure.java
@@ -1,5 +1,10 @@
 package Algorithm;
 
+/**
+ * you can change the implementation or internal structure
+ * in whatever way you like
+ * same for the ImageNodeStructure class
+ */
 public class ImageNodeStructure {
 	int[] edges;
 	int pixelValue;
--- src/Algorithm/ImageStructure.java
+++ src/Algorithm/ImageStructure.java
@@ -5,17 +5,33 @@ import java.awt.image.BufferedImage;
 import java.util.Vector;
 
 public class ImageStructure {
-	ImageNodeStructure[][] pixels;
+	ImageNodeStructure[][] pixels; //see its definition
 	
+	/**
+	 * However you make the structure
+	 * there should be a constructor with an input image
+	 * @param i is the image I loaded
+	 */
 	ImageStructure(BufferedImage i) {
 		
 	}
 	
-	
+	/**
+	 * 
+	 * @param StartX
+	 * @param StartY
+	 * @param endX
+	 * @param endY
+	 * @return in the form of {startnode, next, next ... endnode}
+	 */
 	int[] getShortestPath(int StartX, int StartY, int endX, int endY) {
 		return null;
 	}
 	
+	/**
+	 * this is only implemented when necessary
+	 * @return
+	 */
 	int getPathLength() {
 		return 0;
 	}
